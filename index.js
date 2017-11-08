6ae1bb9cb87dea67298178dcbd060c9670dc0c5f
Ryan Sabo <sabora920@gmail.com>
2017-11-08 15:13:33 -0500

    got generateHTML and render functions working

--- index.js
+++ index.js
@@ -1,21 +1,203 @@
 'use strict';
 
 const STORE = {
-  results: ['daisy',27,'gopher']
+  results: [
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"ld9biNPKjAjgjV7EZ4EKeEGrhao/h7LqypPLmSoGcvHsXnvZmFcglkE\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "0pdCW9-eiVU"
+      },
+      "snippet": {
+        "publishedAt": "2017-02-11T00:47:40.000Z",
+        "channelId": "UCiiFGfvlKvX3uzMovO3unaw",
+        "title": "BIG WAVE SURFING COMPILATION 2017",
+        "description": "EVER HAD ONE OF THOSE DAYS PART-2 BIG WAVE SURFING, **AMAZING FOOTAGE ** WITH 60-100FT- HUGE SURF Please Subscribe if You Would like ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/0pdCW9-eiVU/default.jpg",
+            "width": 120,
+            "height": 90
+          }, 
+          "medium": {
+            "url": "https://i.ytimg.com/vi/0pdCW9-eiVU/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/0pdCW9-eiVU/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Absolutely Flawless",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"ld9biNPKjAjgjV7EZ4EKeEGrhao/wTrwHzyOOdhChMNAMPPBqeheolM\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "vk0F8dHo3wU"
+      },
+      "snippet": {
+        "publishedAt": "2015-10-14T13:45:47.000Z",
+        "channelId": "UC-Zt7GPzlrPPQexkG9-shPg",
+        "title": "\"Pacific Dreams\" A California Surfing Film",
+        "description": "\"Pacific Dreams\" is a surfing movie featuring my 2015 footage shot around the beautiful state of California. Filmed & Edited by Jeff Chavolla ( http://www.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/vk0F8dHo3wU/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/vk0F8dHo3wU/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/vk0F8dHo3wU/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Jeff Chavolla",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"ld9biNPKjAjgjV7EZ4EKeEGrhao/U34_XAoTV_iG0y2JbxJOYOpVON8\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "b6hoBp7Hk-A"
+      },
+      "snippet": {
+        "publishedAt": "2015-02-03T20:00:01.000Z",
+        "channelId": "UCblfuW_4rakIf2h6aqANefA",
+        "title": "The Beautiful Chaos of Surfing Pipeline",
+        "description": "Pipeline is the Yankee Stadium, center court at Wimbledon, and the Melbourne Cricket Ground of surfing. Surfers grow up on the myths of the hallowed reef ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/b6hoBp7Hk-A/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/b6hoBp7Hk-A/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/b6hoBp7Hk-A/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Red Bull",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"ld9biNPKjAjgjV7EZ4EKeEGrhao/E9c_lB0GRVEOAwziF0ni4Vcq22M\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "Fzg_dKowXuQ"
+      },
+      "snippet": {
+        "publishedAt": "2017-09-18T17:30:00.000Z",
+        "channelId": "UCytZlLB3uBn2XFfXlN25zPg",
+        "title": "SURFING HURRICANE IRMA!! **Deadly Waves**",
+        "description": "Surfing some huge waves in Hurricane Irma! Thanks to Ben Gravy : https://www.youtube.com/user/BenRobNUB SUPPORT THE RELIEF & RESCUE!!",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/Fzg_dKowXuQ/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/Fzg_dKowXuQ/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/Fzg_dKowXuQ/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "JOOGSQUAD PPJT",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"ld9biNPKjAjgjV7EZ4EKeEGrhao/kTSkgU0wvyF4eAtJqB97kxOqMt4\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "t_OZB7gZe9Q"
+      },
+      "snippet": {
+        "publishedAt": "2017-08-30T16:00:07.000Z",
+        "channelId": "UCblfuW_4rakIf2h6aqANefA",
+        "title": "Surfing the monstrous Waves of Ireland",
+        "description": "Get the full story here: http://win.gs/2vDLjHp Want to be one of the best big wave surfers in the world? Then we hope you've been taking some notes from Natxo ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/t_OZB7gZe9Q/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/t_OZB7gZe9Q/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/t_OZB7gZe9Q/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Red Bull",
+        "liveBroadcastContent": "none"
+      }
+    }
+  ],
+  totalResults:0,
+  resultsPerPage:0
 };
+
+
 
 // Get data function 
 
-function getDataFromYoutube () {
+function getDataFromYoutube (input, callback) {
   // retrieve data per search keyword from YouTube API
   // store this data in RESULTS
 }
 
+//transfer to STORE fuction
+
+function transferApiDatatoSTORE(){
+  
+}
+
 // Generator function
 
-function generateResults () {
+function generateResults (index) {
   // pull search results data object(s) from RESULTS
   // plug into HTML template
+  let thumbnailURL = index.snippet.thumbnails.default.url
+  return `
+  <div>
+    <img src="${thumbnailURL}">
+  </div>
+`;
 }
 
 
@@ -24,6 +206,12 @@ function generateResults () {
 function renderSearchResults () {
   // Call generator function, grab returned html string
   // render to DOM
+  let results = [];
+  for(let i=0; i<STORE.results.length; i++){
+    let c = generateResults(STORE.results[i]);
+    results.push(c);
+  }
+  $('.js-search-results').html(results);
 }
 
 // Event handler function for form submit
@@ -37,9 +225,16 @@ function handleUserSubmit () {
     // clear the input
     console.log(query);
     queryTarget.val('');
+    getDataFromYoutube(query, );
   });
 }
 
 // DOM Ready handle function
 
-$(handleUserSubmit);
+function main(){
+  handleUserSubmit();
+  renderSearchResults();
+}
+
+
+$(main);
